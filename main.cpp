b30468c2e0bcfd252d324e5ea914c4b37458a6a8
linyfivan <lin.yf.ivan@gmail.com>
2024-03-27 17:35:05 +0800

    Add more test file; Use GiNac library

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "z3++.h"
 #include <unordered_map>
 
+#include <ginac/ginac.h>
+#include <z3_api.h>
+using namespace GiNaC;
+
 
 using namespace z3;
 using namespace std;
@@ -17,9 +21,12 @@ void extract(expr const & e, expr_vector* ar_formulae, params p) {
         //cout << "application of " << f.name() << f.decl_kind() << ": " << e << "\n";
         if (archeck.find(f.decl_kind()) != archeck.end()) {
             auto e_simp = e.simplify(p);
-            //cout << e << endl;
-            //cout << e_simp << endl;
-            ar_formulae->push_back(e_simp);
+            //cout << "e: " <<e << endl;
+            //cout << "e simp: " <<e_simp << endl;
+            if (!(e_simp.is_true() || e_simp.is_false())) {
+                ar_formulae->push_back(e_simp);
+            }
+            
         }
         else {
             unsigned num = e.num_args();
@@ -38,21 +45,38 @@ void extract(expr const & e, expr_vector* ar_formulae, params p) {
     }
 }
 
+/*
+*convert z3 const to ginac numeric (+-)
+*
+*/
+numeric var_to_num(const expr) {
+    
+}
+
+
 void exprs_to_matrix(const expr_vector *exprs) {
     for(const auto e : *exprs) {
         
-        unordered_map<string ,int> coef_map ;
-        
+        unordered_map<string ,numeric> coef_map ;
         auto f = (e.decl().decl_kind() == Z3_OP_NOT) ? e.arg(0) : e;
-        
+    
         assert(f.num_args() == 2);
+        cout<<"expr: "<<f<<endl;
         auto monomials =  f.arg(0);
+        cout<<"monomials: "<<monomials<<endl;
         
-        assert(monomials.decl().decl_kind() == Z3_OP_ADD);
         auto constant = f.arg(1);
-        for(unsigned i=0; i<monomials.num_args(); i++) {
-            auto x = monomials.arg(i);
-            //cout<<x<<endl;
+        cout<<"constants: "<<constant<<endl;
+
+        /*
+        * case 1: (>= x 0)
+        * case 2: (>= (* (-1) x) 0)
+        * case 3: (>= (+ (* (-1) x) y z) 0)
+        *
+        */
+
+        if(monomials.decl().decl_kind() == Z3_OP_ADD) {
+            auto x = monomials;
             if(x.is_const()) {
                 cout<<x<< "is"<<x.decl().name().str()<<endl;
                 coef_map[x.decl().name().str()] = 1;
@@ -64,9 +88,29 @@ void exprs_to_matrix(const expr_vector *exprs) {
                 //FIXME:deal with negative number 
             }
         }
+        else {
+            assert(monomials.decl().decl_kind() == Z3_OP_ADD);
+            for(unsigned i=0; i<monomials.num_args(); i++) {
+                auto x = monomials.arg(i);
+                //cout<<x<<endl;
+                if(x.is_const()) {
+                    cout<<x<< "is"<<x.decl().name().str()<<endl;
+                    coef_map[x.decl().name().str()] = 1;
+                }
+                else {
+                    assert(x.num_args() == 2);
+                    assert(x.arg(1).is_const());
+                    coef_map[x.arg(1).decl().name().str()] = x.arg(0).decl().name().to_int();
+                    //FIXME:deal with negative number 
+                }
+            }
+        }
+        
 
     }
 }
+
+
 
 void tst_visit() {
     cout << "visit example\n";
